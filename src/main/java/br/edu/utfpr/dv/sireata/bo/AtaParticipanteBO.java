244e6458c89276639a80bcd65871ca1f8ed84a66
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2022-05-31 16:09:22 -0300

    Criando Ligação de ModelBO e Product
    Criando Ligação de ModelBO e Product

--- src/main/java/br/edu/utfpr/dv/sireata/bo/AtaParticipanteBO.java
+++ src/main/java/br/edu/utfpr/dv/sireata/bo/AtaParticipanteBO.java
@@ -5,78 +5,77 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import br.edu.utfpr.dv.sireata.dao.AtaParticipanteDAO;
+import br.edu.utfpr.dv.sireata.dao.ModelDAO;
+import br.edu.utfpr.dv.sireata.dao.ModelDAO;
 import br.edu.utfpr.dv.sireata.model.AtaParticipante;
 
-public class AtaParticipanteBO {
-	
-	public AtaParticipante buscarPorId(int id) throws Exception{
-		try{
-			AtaParticipanteDAO dao = new AtaParticipanteDAO();
-			
-			return dao.buscarPorId(id);
-		}catch(Exception e){
+public class AtaParticipanteBO extends ModelBO {
+
+	public AtaParticipante buscarPorId(int id) throws Exception {
+		try {
+
+			return getDAO().buscarPorId(id);
+		} catch (Exception e) {
 			Logger.getGlobal().log(Level.SEVERE, e.getMessage(), e);
-			
+
 			throw new Exception(e.getMessage());
 		}
 	}
-	
-	public List<AtaParticipante> listarPorAta(int idAta) throws Exception{
-		try{
-			AtaParticipanteDAO dao = new AtaParticipanteDAO();
-			
-			return dao.listarPorAta(idAta);
-		}catch(Exception e){
+
+	public List<AtaParticipante> listarPorAta(int idAta) throws Exception {
+		try {
+
+			return new AtaParticipanteDAO().listarPorAta(idAta);
+		} catch (Exception e) {
 			Logger.getGlobal().log(Level.SEVERE, e.getMessage(), e);
-			
+
 			throw new Exception(e.getMessage());
 		}
 	}
-	
-	public void validarDados(AtaParticipante participante) throws Exception{
-		if((participante.getParticipante() == null) || (participante.getParticipante().getIdUsuario() == 0)){
+
+	public void validarDados(AtaParticipante participante) throws Exception {
+		if ((participante.getParticipante() == null) || (participante.getParticipante().getIdUsuario() == 0)) {
 			throw new Exception("Informe o usuário.");
 		}
-		if((!participante.isPresente()) && (participante.getMotivo().trim().isEmpty())){
+		if ((!participante.isPresente()) && (participante.getMotivo().trim().isEmpty())) {
 			throw new Exception("Informe o motivo da ausência.");
 		}
-		if(participante.isPresente()){
+		if (participante.isPresente()) {
 			participante.setMotivo("");
 		}
 	}
-	
-	public int salvar(AtaParticipante participante) throws Exception{
-		try{
-			if((participante.getAta() == null) || (participante.getAta().getIdAta() == 0)){
+
+	public int salvar(AtaParticipante participante) throws Exception {
+		try {
+			if ((participante.getAta() == null) || (participante.getAta().getIdAta() == 0)) {
 				throw new Exception("Informe a ata.");
 			}
-			
+
 			this.validarDados(participante);
-			
-			AtaParticipanteDAO dao = new AtaParticipanteDAO();
-			
-			return dao.salvar(participante);
-		}catch(Exception e){
+
+			return getDAO().salvar(participante);
+		} catch (Exception e) {
 			Logger.getGlobal().log(Level.SEVERE, e.getMessage(), e);
-			
+
 			throw new Exception(e.getMessage());
 		}
 	}
-	
-	public void excluir(AtaParticipante participante) throws Exception{
-		this.excluir(participante.getIdAtaParticipante());
-	}
-	
-	public void excluir(int id) throws Exception{
-		try{
-			AtaParticipanteDAO dao = new AtaParticipanteDAO();
-			
-			dao.excluir(id);
-		}catch(Exception e){
+
+	public void excluir(int id) throws Exception {
+		try {
+
+			new AtaParticipanteDAO().excluir(id);
+		} catch (Exception e) {
 			Logger.getGlobal().log(Level.SEVERE, e.getMessage(), e);
-			
+
 			throw new Exception(e.getMessage());
 		}
+	}
+
+	@Override
+	public ModelDAO<AtaParticipante> getDAO() {
+		// TODO Auto-generated method stub
+		return new AtaParticipanteDAO();
 	}
 
 }
