de14afd470146267188e961e21b2f4e90ae642ab
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2022-05-31 16:10:19 -0300

    Criando Ligação de ModelBO e Product
    Criando Ligação de ModelBO e Product

--- src/main/java/br/edu/utfpr/dv/sireata/bo/CampusBO.java
+++ src/main/java/br/edu/utfpr/dv/sireata/bo/CampusBO.java
@@ -5,84 +5,86 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import br.edu.utfpr.dv.sireata.dao.CampusDAO;
+import br.edu.utfpr.dv.sireata.dao.ModelDAO;
+import br.edu.utfpr.dv.sireata.dao.ModelDAO;
 import br.edu.utfpr.dv.sireata.model.Campus;
 
-public class CampusBO {
-	
-	public Campus buscarPorId(int id) throws Exception{
-		try{
-			CampusDAO dao = new CampusDAO();
-			
-			return dao.buscarPorId(id);
-		}catch(Exception e){
+public class CampusBO extends ModelBO {
+
+	public Campus buscarPorId(int id) throws Exception {
+		try {
+
+			return getDAO().buscarPorId(id);
+		} catch (Exception e) {
 			Logger.getGlobal().log(Level.SEVERE, e.getMessage(), e);
-			
+
 			throw new Exception(e.getMessage());
 		}
 	}
-	
-	public Campus buscarPorDepartamento(int idDepartamento) throws Exception{
-		try{
-			CampusDAO dao = new CampusDAO();
-			
-			return dao.buscarPorDepartamento(idDepartamento);
-		}catch(Exception e){
+
+	public Campus buscarPorDepartamento(int idDepartamento) throws Exception {
+		try {
+
+			return new CampusDAO().buscarPorDepartamento(idDepartamento);
+		} catch (Exception e) {
 			Logger.getGlobal().log(Level.SEVERE, e.getMessage(), e);
-			
+
 			throw new Exception(e.getMessage());
 		}
 	}
-	
-	public List<Campus> listarTodos(boolean apenasAtivos) throws Exception{
-		try{
-			CampusDAO dao = new CampusDAO();
-			
-			return dao.listarTodos(apenasAtivos);
-		}catch(Exception e){
+
+	public List<Campus> listarTodos(boolean apenasAtivos) throws Exception {
+		try {
+
+			return new CampusDAO().listarTodos(apenasAtivos);
+		} catch (Exception e) {
 			Logger.getGlobal().log(Level.SEVERE, e.getMessage(), e);
-			
+
 			throw new Exception(e.getMessage());
 		}
 	}
-	
-	public List<Campus> listarParaCriacaoAta(int idUsuario) throws Exception{
-		try{
-			CampusDAO dao = new CampusDAO();
-			
-			return dao.listarParaCriacaoAta(idUsuario);
-		}catch(Exception e){
+
+	public List<Campus> listarParaCriacaoAta(int idUsuario) throws Exception {
+		try {
+
+			return new CampusDAO().listarParaCriacaoAta(idUsuario);
+		} catch (Exception e) {
 			Logger.getGlobal().log(Level.SEVERE, e.getMessage(), e);
-			
+
 			throw new Exception(e.getMessage());
 		}
 	}
-	
-	public List<Campus> listarParaConsultaAtas(int idUsuario) throws Exception{
-		try{
-			CampusDAO dao = new CampusDAO();
-			
-			return dao.listarParaConsultaAtas(idUsuario);
-		}catch(Exception e){
+
+	public List<Campus> listarParaConsultaAtas(int idUsuario) throws Exception {
+		try {
+
+			return new CampusDAO().listarParaConsultaAtas(idUsuario);
+		} catch (Exception e) {
 			Logger.getGlobal().log(Level.SEVERE, e.getMessage(), e);
-			
+
 			throw new Exception(e.getMessage());
 		}
 	}
-	
-	public int salvar(Campus campus) throws Exception{
-		if(campus.getNome().isEmpty()){
+
+	public int salvar(Campus campus) throws Exception {
+		if (campus.getNome().isEmpty()) {
 			throw new Exception("Informe o nome do câmpus.");
 		}
-		
-		try{
-			CampusDAO dao = new CampusDAO();
-			
-			return dao.salvar(campus);
-		}catch(Exception e){
+
+		try {
+
+			return getDAO().salvar(campus);
+		} catch (Exception e) {
 			Logger.getGlobal().log(Level.SEVERE, e.getMessage(), e);
-			
+
 			throw new Exception(e.getMessage());
 		}
+	}
+
+	@Override
+	public ModelDAO<Campus> getDAO() {
+		// TODO Auto-generated method stub
+		return new CampusDAO();
 	}
 
 }
