16d02b38530963aec375e3b41ba8d17a65100bea
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2022-05-31 15:44:58 -0300

    Gerando e importando Métodos
    Criando em BO e importando Métodos

---
+++ src/main/java/br/edu/utfpr/dv/sireata/bo/ModelBO.java
@@ -0,0 +1,47 @@
+package br.edu.utfpr.dv.sireata.bo;
+
+import br.edu.utfpr.dv.sireata.dao.AtaDAO;
+import br.edu.utfpr.dv.sireata.dao.ModelDAO;
+
+public abstract class ModelBO<E> {
+
+    public enum BOS {
+        ANEXODAO,
+        ATADAO,
+        ATAPARTICIPANTEDAO,
+        CAMPUSDAO,
+        COMENTARIODAO,
+        DEPARTAMENTODAO,
+        ORGAODAO,
+        PAUTADAO,
+        USUARIODAO
+
+    }
+    public static ModelDAO from(BOS dataRetriever) {
+        
+        switch (dataRetriever) {
+            case ANEXODAO:
+                return new br.edu.utfpr.dv.sireata.bo.AnexoBO().getDAO();
+            case ATADAO:
+                return new br.edu.utfpr.dv.sireata.bo.AtaBO().getDAO();
+            case ATAPARTICIPANTEDAO:
+                return new br.edu.utfpr.dv.sireata.bo.AtaParticipanteBO().getDAO();
+            case CAMPUSDAO:
+                return new br.edu.utfpr.dv.sireata.bo.CampusBO().getDAO();
+            case COMENTARIODAO:
+                return new br.edu.utfpr.dv.sireata.bo.ComentarioBO().getDAO();
+            case DEPARTAMENTODAO:
+                return new br.edu.utfpr.dv.sireata.bo.DepartamentoBO().getDAO();
+            case ORGAODAO:
+                return new br.edu.utfpr.dv.sireata.bo.OrgaoBO().getDAO();
+            case PAUTADAO:
+                return new br.edu.utfpr.dv.sireata.bo.PautaBO().getDAO();
+            case USUARIODAO:
+                return new br.edu.utfpr.dv.sireata.bo.UsuarioBO().getDAO();
+            default:
+                return null;
+        }
+    }
+    public abstract ModelDAO getDAO();
+
+}
